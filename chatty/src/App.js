b79d40460b8bb2a090f7345246fc702a4b5cc9a6
y0shiki <63571264+y0shiki@users.noreply.github.com>
2023-03-18 02:14:22 -0700

    add the chatbot UI and functionality

--- chatty/src/App.js
+++ chatty/src/App.js
@@ -1,12 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react";
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hey, I am Chatty!",
+      sender: "Chatty"
+
+    }
+  ])
+
+  const handelSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage];
+    //update the message state
+    setMessages(newMessages);
+
+    //process message to ChatGPT 
+  }
   return (
     <div className="App">
-      <header>
-        <h1>CHATTY</h1>
-      </header>
+      <div className='Head'>
+        <header>
+          <h1>CHATTY</h1>
+        </header>
+      </div>
+
+      <div style={{height: "100%", width: "100%"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handelSend} />
+          </ChatContainer>
+        </MainContainer>
+
+      </div>
+
     </div>
   );
 }
